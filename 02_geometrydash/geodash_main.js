525adf8c54360414ce9ac580b29f74212a295628
jovanohks <jovanoh400@gmail.com>
2026-07-02 19:22:22 +0800

    Initial commit

---
+++ 02_geometrydash/geodash_main.js
@@ -0,0 +1,40 @@
+//player box
+
+
+// game variables
+
+
+// world building groups
+
+
+// image sprites
+
+
+// menu
+
+
+// sound assets
+
+
+function preload() {
+
+}
+
+function setup() {
+  
+}
+
+function draw() {
+  
+}
+
+
+
+
+
+
+
+
+
+
+
