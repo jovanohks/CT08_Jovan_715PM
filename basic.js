71a3e4a78a1c4113ec4a54eaae6faa4c6076545c
jovanohks <jovanoh400@gmail.com>
2026-07-09 11:38:07 +0000

    Jul 9, 2026, 11:38 AM

--- basic.js
+++ basic.js
@@ -1,8 +1,8 @@
 function setup(){
-    new canvas(800,400)
-    console.log("hello world")
+    new canvas(800,400);
+    console.log("hello world");
 
-    let a=1
+    let a=1;
 }
 function draw(){
 
