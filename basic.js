9516dee7981ce030a44c6ba9e935921a926da7ed
jovanohks <jovanoh400@gmail.com>
2026-07-09 11:58:24 +0000

    Jul 9, 2026, 11:58 AM

--- basic.js
+++ basic.js
@@ -8,7 +8,7 @@ function setup(){
     console.log("area:",area);
     text("area:"+area,100,100);
     fill(0);
-    for (let i=0; i<10; i++){
+    for (let i=10; i>0 ; i++){
         console.log(i);
     }
 }
