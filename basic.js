7dd20517fd6aeb6e2548acd81126dd094a466977
jovanohks <jovanoh400@gmail.com>
2026-07-09 11:49:01 +0000

    Jul 9, 2026, 11:49 AM

--- basic.js
+++ basic.js
@@ -1,10 +1,11 @@
 function setup(){
-    new canvas(800,400);
+    new Canvas(800,400);
     background(220);
     console.log("hello world");
     let a=1;
     let b=2;
     let sum=a+b;
+    console.log()
 }
 function draw(){
 
