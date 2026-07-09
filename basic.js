6d8451655a6a7796325f6efb7c84dd41a037a939
jovanohks <jovanoh400@gmail.com>
2026-07-09 11:49:23 +0000

    Jul 9, 2026, 11:49 AM

--- basic.js
+++ basic.js
@@ -2,9 +2,9 @@ function setup(){
     new Canvas(800,400);
     background(220);
     console.log("hello world");
-    let a=1;
-    let b=2;
-    let area=0.5*a+b;
+    let a=10;
+    let b=5;
+    let area=0.5*a*b;
     console.log("area:",area);
 }
 function draw(){
