4fc59467d40592e18769710aff2a99a75f11a144
jovanohks <jovanoh400@gmail.com>
2026-07-23 11:23:23 +0000

    Jul 23, 2026, 11:23 AM

--- 02_geometrydash/geodash_main.js
+++ 02_geometrydash/geodash_main.js
@@ -34,6 +34,7 @@ function setup() {
     startCoordinate=[50,height-TILE_SIZE/2];
     player.x=startCoordinate[0];
     player.y=startCoordinate[1];
+    ground= new Group()
 }
 
 function draw() {
