e1e6dffc9dac481b9dbe38fa623401a1aadbbf70
jovanohks <jovanoh400@gmail.com>
2026-07-23 11:36:05 +0000

    Jul 23, 2026, 11:36 AM

--- 02_geometrydash/geodash_main.js
+++ 02_geometrydash/geodash_main.js
@@ -20,7 +20,7 @@ function preload() {
     box=loadImage("assets/cube.png");
     bg=loadImage("assets/geobg.png");
     tileMap1=loadStrings("stages/tiles1.txt")
-    spike
+    spike=loadImage("assets/spikes.png")
 }
 
 function setup() {
