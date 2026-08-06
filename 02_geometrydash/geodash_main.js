290e01a4ca77a2d1cd24c8caf793150eb7220c8d
jovanohks <jovanoh400@gmail.com>
2026-08-06 12:25:28 +0000

    Aug 6, 2026, 12:25 PM

--- 02_geometrydash/geodash_main.js
+++ 02_geometrydash/geodash_main.js
@@ -130,6 +130,7 @@ function triggerGameOver(){
         endSprite=new Sprite(player.x,height/2,126,24);
         endSprite.collider="none";
         endSprite.img=endGameImg;
+        backgroundtrack.stop()
     }
 }
 function resetGame(){
