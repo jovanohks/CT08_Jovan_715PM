179efa5e17df7f7662b11b7a760e585023f5ac3d
jovanohks <jovanoh400@gmail.com>
2026-08-06 12:02:16 +0000

    Aug 6, 2026, 12:02 PM

--- 02_geometrydash/geodash_main.js
+++ 02_geometrydash/geodash_main.js
@@ -156,7 +156,7 @@ function draw() {
         clear();
         image(bg,0,0,800,600);
         player.vel.x=8;
-        if (framecount %3 ===0)
+        if (framecount %3 ===0 && box.colliding(ground) &&box.)
         for (let tile of ground){
             if (player.collides(tile)){
                 let leftEdge=tile.w-tile.h/2;
