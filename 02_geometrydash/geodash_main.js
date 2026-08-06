4fa692ee5229b0e9cdb453515fbe4bed815b490f
jovanohks <jovanoh400@gmail.com>
2026-08-06 12:23:14 +0000

    Aug 6, 2026, 12:23 PM

--- 02_geometrydash/geodash_main.js
+++ 02_geometrydash/geodash_main.js
@@ -164,6 +164,9 @@ function draw() {
         clear();
         image(bg,0,0,800,600);
         player.vel.x=8;
+        if (background.elt.paused){
+            
+        }
         if (framecount %3 ===0 && box.colliding(ground) &&box.vel.x>=0.5){
             box.rotation=0;
             let particle = new Sprite(box.x,box.y+box.h/2 ,8,8,"none");
