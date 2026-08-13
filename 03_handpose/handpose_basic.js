1b36f7588419d9f741fcd930010b9d3bda77a651
jovanohks <jovanoh400@gmail.com>
2026-08-13 12:22:18 +0000

    Aug 13, 2026, 12:22 PM

---
+++ 03_handpose/handpose_basic.js
@@ -0,0 +1,33 @@
+//=========================================
+// Variables
+//=========================================
+let handpose;
+let videoW=640;
+let videoH=480
+//=========================================
+// Code
+//=========================================
+
+function preload() {
+    let options = {
+        flipped:true,
+        runtime:"tfjs",
+        modelType:"full",
+        detectorModelUrl:"undefined",
+        landmarkModelUrl:"undefined",
+
+    }
+    handPose=ml5.handPose(options);
+
+}
+
+function setup() {
+    createCanvas(videoW,videoH);
+    
+}
+
+function draw() {}
+
+//=========================================
+// Function Created
+//=========================================
