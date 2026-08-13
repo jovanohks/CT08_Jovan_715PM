d13d27ccb40f5c94340eada68df93e74e504771f
jovanohks <jovanoh400@gmail.com>
2026-08-13 12:30:42 +0000

    Aug 13, 2026, 12:30 PM

--- 03_handpose/handpose_basic.js
+++ 03_handpose/handpose_basic.js
@@ -38,7 +38,7 @@ function setup() {
     video=createCapture(constraints);
     video.size=(640,480);
     video.hide();
-    handPose.detectStart(video,)
+    handPose.detectStart(video,gotHands);
 }
 
 function draw() {}
