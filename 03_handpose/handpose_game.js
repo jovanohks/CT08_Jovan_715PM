9bd79fc786a143586dc64c93e1280fc5e7f96e19
jovanohks <jovanoh400@gmail.com>
2026-08-20 12:33:41 +0000

    Aug 20, 2026, 12:33 PM

---
+++ 03_handpose/handpose_game.js
@@ -0,0 +1,63 @@
+//=========================================
+// Variables
+//=========================================
+let hands=[]
+let handPose;
+let videoW=640;
+let videoH=480;
+let video;
+//=========================================
+// Code
+//=========================================
+
+function preload() {
+    let options = {
+        flipped:true,
+        runtime:"tfjs",
+        modelType:"full",
+        detectorModelUrl:"undefined",
+        landmarkModelUrl:"undefined",
+
+    }
+    handPose=ml5.handPose(options);
+
+}
+function gotHands(results){
+    hands=results;
+
+}
+function setup() {
+    createCanvas(videoW,videoH);
+    let constraints={
+        video:{
+            mandatory:{
+                minWidth:videoW,
+                minHeight:videoH
+            },
+            optional: [{minFrameRate:60}],
+        },
+        audio: false,
+
+        flipped:true,
+    }
+    video=createCapture(constraints);
+    video.size=(640,480);
+
+    handPose.detectStart(video,gotHands);
+}
+
+function draw() {
+    image(video,0,0,videoW,videoH);
+    for (let i=0; i<hands.length; i++){
+        let hand=hands[i];
+        for (let j=0; j<hand.keypoints.length; j++){
+            let keypoint=hand.keypoints[j];
+            circle(keypoint.x,keypoint.y,10)
+        }
+    }
+
+}
+
+//=========================================
+// Function Created
+//=========================================
