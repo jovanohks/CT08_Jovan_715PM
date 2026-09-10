a5bfd9f6a87a0bc95349beb24b1e82bce752674f
jovanohks <jovanoh400@gmail.com>
2026-09-10 12:42:12 +0000

    Sep 10, 2026, 12:42 PM

--- 03_handpose/handpose_game.js
+++ 03_handpose/handpose_game.js
@@ -176,7 +176,9 @@ function draw() {
         text("bounce the ball",width/2,height*0.1);
         textSize(32);
         text("Use index finger to bounce the ball!",width/2, height*0.2);
-        text("Press space to start game")
+        text("Press space to start game",width/2,height*0.3)
+    }else{
+        
     }
 // Draw all the tracked hand points
 // Loop through all the hands detected (can detect left or right)
