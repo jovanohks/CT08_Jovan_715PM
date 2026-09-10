b16486113907c9825681d40804fddc9426a82bee
jovanohks <jovanoh400@gmail.com>
2026-09-10 12:43:12 +0000

    Sep 10, 2026, 12:43 PM

--- 03_handpose/handpose_game.js
+++ 03_handpose/handpose_game.js
@@ -179,15 +179,15 @@ function draw() {
         text("Press space to start game",width/2,height*0.3)
     }else{
         for (let i = 0; i < hands.length; i++) {
-        let hand = hands[i]; // current hand (left or right)
+            let hand = hands[i]; // current hand (left or right)
 
-    // Keypoint 8 = INDEX_FINGER_TIP
-        let keypoint = hand.keypoints[8];
+        // Keypoint 8 = INDEX_FINGER_TIP
+            let keypoint = hand.keypoints[8];
 
-    // for every keypoint, draw a circle.
-        fingerTip.x = keypoint.x;
-        fingerTip.y = keypoint.y;
-    }      
+        // for every keypoint, draw a circle.
+            fingerTip.x = keypoint.x;
+            fingerTip.y = keypoint.y;
+        }      
     }
 // Draw all the tracked hand points
 // Loop through all the hands detected (can detect left or right)
