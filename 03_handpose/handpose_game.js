dd540c323ca57bbda37bde10b3ad0489462779de
jovanohks <jovanoh400@gmail.com>
2026-09-10 12:29:58 +0000

    Sep 10, 2026, 12:29 PM

--- 03_handpose/handpose_game.js
+++ 03_handpose/handpose_game.js
@@ -171,7 +171,8 @@ function draw() {
     image(video, 0, 0, videoW, videoH);
     if (gameStarted===false){
         textSize(28);
-        textAlign(CENTER,CENTER)
+        textAlign(CENTER,CENTER );
+        
     }
 // Draw all the tracked hand points
 // Loop through all the hands detected (can detect left or right)
