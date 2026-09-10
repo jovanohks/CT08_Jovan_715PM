1c1c6b0ea7e2fb03a3be0e3db2eabea64f1fe390
jovanohks <jovanoh400@gmail.com>
2026-09-10 12:04:17 +0000

    Sep 10, 2026, 12:04 PM

--- 03_handpose/handpose_game.js
+++ 03_handpose/handpose_game.js
@@ -146,8 +146,8 @@ function setup() {
     wall1= new Sprite();
     wall1.width=20;
     wall1.height=2000;
-    wall1.x=0
-    wall1.y=0
+    wall1.x=-20;
+    wall1.y=0;
 
     wall1.collider="static";
 }
