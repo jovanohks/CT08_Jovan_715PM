bff7948dc8f85fff4b51f934450ec1e8a6d599ae
jovanohks <jovanoh400@gmail.com>
2026-09-17 12:47:27 +0000

    Sep 17, 2026, 12:47 PM

--- 04_bodypose_game/pose_matching_game.js
+++ 04_bodypose_game/pose_matching_game.js
@@ -76,6 +76,7 @@ function draw() {
     // Draw the middle line that separates Player 1 and Player 2 areas.
     drawMiddleLine();
 
+    Image(video,came)
 }
 
 // ====================================================
