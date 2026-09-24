79db75be25e172ae99ddd77009a5de3bd8422d61
jovanohks <jovanoh400@gmail.com>
2026-09-24 11:52:28 +0000

    Sep 24, 2026, 11:52 AM

--- 04_bodypose_game/pose_matching_game.js
+++ 04_bodypose_game/pose_matching_game.js
@@ -134,6 +134,6 @@ function gotPoses(results){
 function drawDetectionStatus(){
     fill(0);
     textSize(24);
-    text("People detected: "+detectedPeople)
+    text("People detected: "+detectedPeople.length,width/2,)
 
 }
