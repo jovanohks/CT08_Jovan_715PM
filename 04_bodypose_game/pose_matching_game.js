b6af2da124c018fc7fe12a9440ccfebeeec9c041
jovanohks <jovanoh400@gmail.com>
2026-09-24 12:23:50 +0000

    Sep 24, 2026, 12:23 PM

--- 04_bodypose_game/pose_matching_game.js
+++ 04_bodypose_game/pose_matching_game.js
@@ -141,6 +141,7 @@ function drawSkeleton(person, skeletonColor) {
 function drawAllskeletons(){
     for (let i =0; i < detectedPeople.length;i++){
         let person=detectedPeople[i]
+        drawsle
     }
 }
 // ====================================================
